a52cadaa0c9d140152dae8321394b27d9b9525b0
chetsadaphon <114697178+kihwchetsada@users.noreply.github.com>
2024-08-09 01:29:59 +0700

    update

--- Index.php
+++ Index.php
@@ -4,31 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>โปรเจค</title>
-    <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        table, th, td {
-            border: 1px solid black;
-        }
-        th, td {
-            padding: 8px;
-            text-align: center;
-        }
-        th {
-            background-color: #f2f2f2;
-        }
-        input[type="number"] {
-            width: 90%;
-            padding: 4px;
-            box-sizing: border-box;
-        }
-    </style>
+    <link rel="stylesheet" href="se.css" />
 </head>
 <body>
     <h1>เส้นทางวิกฤต</h1>
-    
+    <form>
     <table>
         <thead>
             <tr>
@@ -43,53 +23,57 @@
         <tbody>
             <tr>
                 <td>A</td>
-                <td><input type="number" name="duration_A"></td>
-                <td><input type="number" name="es_A"></td>
-                <td><input type="number" name="ef_A"></td>
-                <td><input type="number" name="ls_A"></td>
-                <td><input type="number" name="lf_A"></td>
+                <td><input type="number" name="duration_A" class="input"></td>
+                <td><input type="number" name="es_A" class="input"></td>
+                <td><input type="number" name="ef_A" class="input"></td>
+                <td><input type="number" name="ls_A" class="input"></td>
+                <td><input type="number" name="lf_A" class="input"></td>
             </tr>
             <tr>
                 <td>B</td>
-                <td><input type="number" name="duration_B"></td>
-                <td><input type="number" name="es_B"></td>
-                <td><input type="number" name="ef_B"></td>
-                <td><input type="number" name="ls_B"></td>
-                <td><input type="number" name="lf_B"></td>
+                <td><input type="number" name="duration_B" class="input"></td>
+                <td><input type="number" name="es_B" class="input"></td>
+                <td><input type="number" name="ef_B" class="input"></td>
+                <td><input type="number" name="ls_B" class="input"></td>
+                <td><input type="number" name="lf_B" class="input"></td>
             </tr>
             <tr>
                 <td>C</td>
-                <td><input type="number" name="duration_C"></td>
-                <td><input type="number" name="es_C"></td>
-                <td><input type="number" name="ef_C"></td>
-                <td><input type="number" name="ls_C"></td>
-                <td><input type="number" name="lf_C"></td>
+                <td><input type="number" name="duration_C" class="input"></td>
+                <td><input type="number" name="es_C" class="input"></td>
+                <td><input type="number" name="ef_C" class="input"></td>
+                <td><input type="number" name="ls_C" class="input"></td>
+                <td><input type="number" name="lf_C" class="input"></td>
             </tr>
             <tr>
                 <td>D</td>
-                <td><input type="number" name="duration_D"></td>
-                <td><input type="number" name="es_D"></td>
-                <td><input type="number" name="ef_D"></td>
-                <td><input type="number" name="ls_D"></td>
-                <td><input type="number" name="lf_D"></td>
+                <td><input type="number" name="duration_D" class="input"></td>
+                <td><input type="number" name="es_D" class="input"></td>
+                <td><input type="number" name="ef_D" class="input"></td>
+                <td><input type="number" name="ls_D" class="input"></td>
+                <td><input type="number" name="lf_D" class="input"></td>
             </tr>
             <tr>
                 <td>E</td>
-                <td><input type="number" name="duration_E"></td>
-                <td><input type="number" name="es_E"></td>
-                <td><input type="number" name="ef_E"></td>
-                <td><input type="number" name="ls_E"></td>
-                <td><input type="number" name="lf_E"></td>
+                <td><input type="number" name="duration_E" class="input"></td>
+                <td><input type="number" name="es_E" class="input"></td>
+                <td><input type="number" name="ef_E" class="input"></td>
+                <td><input type="number" name="ls_E" class="input"></td>
+                <td><input type="number" name="lf_E" class="input"></td>
             </tr>
             <tr>
                 <td>F</td>
-                <td><input type="number" name="duration_F"></td>
-                <td><input type="number" name="es_F"></td>
-                <td><input type="number" name="ef_F"></td>
-                <td><input type="number" name="ls_F"></td>
-                <td><input type="number" name="lf_F"></td>
+                <td><input type="number" name="duration_F" class="input"></td>
+                <td><input type="number" name="es_F" class="input"></td>
+                <td><input type="number" name="ef_F" class="input"></td>
+                <td><input type="number" name="ls_F" class="input"></td>
+                <td><input type="number" name="lf_F" class="input"></td>
             </tr>
         </tbody>
     </table>
+    </form>
 </body>
+<footer>
+<button> Click Me! </button>
+</footer>
 </html>
